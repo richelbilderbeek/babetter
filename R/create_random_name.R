9af5c93bb4679032ebce9c85860077560e93dff7
richelbilderbeek <richel@richelbilderbeek.nl>
2018-11-11 13:40:51 +0100

    Add create_random_x functions from beautier

---
+++ R/create_random_name.R
@@ -0,0 +1,9 @@
+#' Create a random name, for example,
+#'   to be used by \code{\link{create_mrca_prior}}
+#' @inheritParams default_params_doc
+#' @return a random name
+#' @author Richel J.C. Bilderbeek
+#' @noRd
+create_random_name <- function() {
+  paste0(sample(x = letters, size = 42, replace = TRUE), collapse = "")
+}
