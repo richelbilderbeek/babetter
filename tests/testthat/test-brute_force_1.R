4c46115e0d63c43042f4ed7cb8310e737c0d8232
richelbilderbeek <richel@richelbilderbeek.nl>
2019-02-14 14:23:55 +0100

    Use new interface

--- tests/testthat/test-brute_force_1.R
+++ tests/testthat/test-brute_force_1.R
@@ -10,7 +10,7 @@ test_that("all combinations", {
         if (runif(n = 1) > 0.01) next
         output_xml_filename <- tempfile()
         beautier::create_beast2_input_file(
-          input_filenames = input_fasta_filename,
+          input_filename = input_fasta_filename,
           output_filename = output_xml_filename,
           site_models = site_model,
           clock_models = clock_model,
