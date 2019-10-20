91e166541b7526168acdc61ee1cb73411c3c50e0
richelbilderbeek <richel@richelbilderbeek.nl>
2019-10-20 15:44:09 +0200

    Test new feature 'mcmc'

---
+++ tests/testthat/test-test_new_features.R
@@ -0,0 +1,14 @@
+test_that("use", {
+
+  text <- beautier::create_beast2_input_from_model(
+    input_filename = beautier::get_beautier_path("test_output_0.fas"),
+    inference_model = beautier::create_inference_model(
+      mcmc = beautier::create_mcmc(
+        chain_length = 1e7,
+        store_every = 1e3,
+        pre_burnin = 1e6
+      )
+    )
+  )
+  expect_true(are_beast2_input_lines_deep(text))
+})
